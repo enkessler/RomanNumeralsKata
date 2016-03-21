0d7ebd59815fcd65c9c2556de2bed5777442d480
Eric Kessler <morrow748@gmail.com>
2016-03-21 01:08:14 -0400

    Refactoring.
    Cleaned up the flow of control so that it is easier to follow.

--- roman_arabic_converter.rb
+++ roman_arabic_converter.rb
@@ -1,72 +1,76 @@
 class RomanArabicConverter
 
   def r_to_a(number)
+    converted_number = ''
 
-    ones_map ={1 => 'I',
-               2 => 'II',
-               3 => 'III',
-               4 => 'IV',
-               5 => 'V',
-               6 => 'VI',
-               7 => 'VII',
-               8 => 'VIII',
-               9 => 'IX'}
+    # Note: will never have a number with more than four digits
+    converted_number.prepend(convert_ones_place(number % 10))
+    number /= 10
+    converted_number.prepend(convert_tens_place(number % 10))
+    number /= 10
+    converted_number.prepend(convert_hundreds_place(number % 10))
+    number /= 10
+    converted_number.prepend(convert_thousands_place(number % 10))
 
-    tens_map ={1 => 'X',
-               2 => 'XX',
-               3 => 'XXX',
-               4 => 'XL',
-               5 => 'L',
-               6 => 'LX',
-               7 => 'LXX',
-               8 => 'LXXX',
-               9 => 'XC'}
-
-    hundreds_map ={1 => 'C',
-                   2 => 'CC',
-                   3 => 'CCC',
-                   4 => 'CD',
-                   5 => 'D',
-                   6 => 'DC',
-                   7 => 'DCC',
-                   8 => 'DCCC',
-                   9 => 'CM'}
-
-    thousands_map ={1 => 'M',
-                    2 => 'MM',
-                    3 => 'MMM'}
+    converted_number
+  end
 
 
-    case
-      when number > 999
-        thousands = thousands_map[number / 1000]
-        number %= 1000
-        hundreds = hundreds_map[number / 100]
-        number %= 100
-        tens = tens_map[number / 10]
-        number %= 10
-        ones = ones_map[number]
+  private
 
-        thousands + (hundreds || '') + (tens || '') + (ones || '')
-      when number > 99
-        hundreds = hundreds_map[number / 100]
-        number %= 100
-        tens = tens_map[number / 10]
-        number %= 10
-        ones = ones_map[number]
 
-        hundreds + (tens || '') + (ones || '')
-      when number > 9
-        tens = tens_map[number / 10]
-        number %= 10
-        ones = ones_map[number]
+  def convert_ones_place(digit)
+    conversion_map ={0 => '',
+                     1 => 'I',
+                     2 => 'II',
+                     3 => 'III',
+                     4 => 'IV',
+                     5 => 'V',
+                     6 => 'VI',
+                     7 => 'VII',
+                     8 => 'VIII',
+                     9 => 'IX'}
 
-        tens + (ones || '')
-      else
-        ones = ones_map[number]
+    conversion_map[digit]
+  end
 
-        ones
-    end
+  def convert_tens_place(digit)
+    conversion_map ={0 => '',
+                     1 => 'X',
+                     2 => 'XX',
+                     3 => 'XXX',
+                     4 => 'XL',
+                     5 => 'L',
+                     6 => 'LX',
+                     7 => 'LXX',
+                     8 => 'LXXX',
+                     9 => 'XC'}
+
+    conversion_map[digit]
+  end
+
+  def convert_hundreds_place(digit)
+    conversion_map ={0 => '',
+                     1 => 'C',
+                     2 => 'CC',
+                     3 => 'CCC',
+                     4 => 'CD',
+                     5 => 'D',
+                     6 => 'DC',
+                     7 => 'DCC',
+                     8 => 'DCCC',
+                     9 => 'CM'}
+
+    conversion_map[digit]
+  end
+
+  def convert_thousands_place(digit)
+    conversion_map ={0 => '',
+                     1 => 'M',
+                     2 => 'MM',
+                     3 => 'MMM'}
+
+    conversion_map[digit]
   end
 
 end

2ea6e317b115d217e6e5b7a64fb29b263d832a13
Eric Kessler <morrow748@gmail.com>
2016-03-21 00:14:33 -0400

    Less trivial uses cases added.
    Got the converter working for numbers up to 10.

--- roman_arabic_converter.rb
+++ roman_arabic_converter.rb
@@ -1,7 +1,18 @@
 class RomanArabicConverter
 
   def r_to_a(number)
-    'I'
+    conversion_map = {1 => 'I',
+                      2 => 'II',
+                      3 => 'III',
+                      4 => 'IV',
+                      5 => 'V',
+                      6 => 'VI',
+                      7 => 'VII',
+                      8 => 'VIII',
+                      9 => 'IX',
+                      10 => 'X'}
+
+    conversion_map[number]
   end
 
 end
--- spec/roman_arabic_converter_spec.rb
+++ spec/roman_arabic_converter_spec.rb
@@ -3,11 +3,47 @@ require_relative '../roman_arabic_converter'
 
 describe 'Converting Roman numerals' do
 
-  let(:converter){RomanArabicConverter.new}
+  let(:converter) { RomanArabicConverter.new }
 
 
   it 'converts 1 to I' do
     expect(converter.r_to_a(1)).to eq('I')
+  end
+
+  it 'converts 2 to II' do
+    expect(converter.r_to_a(2)).to eq('II')
+  end
+
+  it 'converts 3 to III' do
+    expect(converter.r_to_a(3)).to eq('III')
+  end
+
+  it 'converts 4 to IV' do
+    expect(converter.r_to_a(4)).to eq('IV')
+  end
+
+  it 'converts 5 to V' do
+    expect(converter.r_to_a(5)).to eq('V')
+  end
+
+  it 'converts 6 to VI' do
+    expect(converter.r_to_a(6)).to eq('VI')
+  end
+
+  it 'converts 7 to VII' do
+    expect(converter.r_to_a(7)).to eq('VII')
+  end
+
+  it 'converts 8 to VIII' do
+    expect(converter.r_to_a(8)).to eq('VIII')
+  end
+
+  it 'converts 9 to IX' do
+    expect(converter.r_to_a(9)).to eq('IX')
+  end
+
+  it 'converts 10 to X' do
+    expect(converter.r_to_a(10)).to eq('X')
   end
 
 end
